6ade81f08baad40c3c7390589a4262315be03a08
Kaylin Punotai <kaylin.punotai@gmail.com>
2022-01-19 22:40:12 -0800

    Added projects
    - added project viewer
    - changed color scheme again
    - added links to contact/social media

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-// import PhotoGallery from './PhotoGallery.js';
+import PhotoGallery from './PhotoGallery.js';
+import { IoMailSharp as EmailIcon, IoLogoLinkedin as LinkedInIcon, IoLogoGithub as GithubIcon} from 'react-icons/io5';
 
 function App() {
   return (
@@ -12,36 +13,18 @@ function App() {
         <a href="#experience">Experience</a>
         <a href="#skills">Skills</a>
         <a href="#projects">Projects</a>
+        <a id="resume" href="Files/Resume.pdf" target="_blank" rel="noreferrer" >Resume</a>
       </div>
 
       <div id="contactBar">
-        <a id="resume" href="%PUBLIC_URL%/Files/Resume.pdf" target="_blank" rel="noreferrer">
-          My Resume
-        </a> 
-
-        <a className="icon" href="mailto:kaylin.punotai@gmail.com">
-          <img
-                src= "Images/email-icon.png"
-                alt="kaylin.punotai@gmail.com"
-                width="35px"
-                height="35px"
-          />
+        <a href="mailto:kaylin.punotai@gmail.com">
+          <EmailIcon/>
         </a>
-        <a className="icon" href="https://www.linkedin.com/in/kaylin-punotai/" target="_blank" rel="noreferrer">
-          <img
-                src= "Images/linkedin-icon.png"
-                alt="https://www.linkedin.com/in/kaylin-punotai/"
-                width="35px"
-                height="35px"
-          />
+        <a href="https://github.com/kaylinpunotai" target="_blank" rel="noreferrer">
+          <GithubIcon/>
         </a>
-        <a className="icon" href="https://github.com/kaylinpunotai" target="_blank" rel="noreferrer">
-          <img
-                src= "Images/github-icon.png"
-                alt="https://github.com/kaylinpunotai"
-                width="35px"
-                height="35px"
-          />
+        <a href="https://www.linkedin.com/in/kaylin-punotai/" target="_blank" rel="noreferrer">
+          <LinkedInIcon/>
         </a>
       </div>
 
@@ -62,11 +45,13 @@ function App() {
       <div className="app-section" id="about">
         <div className="header">About</div>
         <div className="content">
-          Hi, I'm Kaylin! I'm a self-taught software engineer with experience in React, 
-          JavaScript, and C#.
+          <div className="content-title">
+            Hi, I'm Kaylin! I'm a self-taught software engineer with experience in React, 
+            JavaScript, and C#.
 
-          I am currently a process engineer at Intel and seeking opportunities to break
-          into the programming field.
+            I am currently a process engineer at Intel and seeking opportunities to break
+            into the programming field.
+          </div>
         </div>
       </div>
 
@@ -75,12 +60,12 @@ function App() {
         <div className="content">
           <div className="content-title">University of Colorado Boulder</div>
           <div className="content-sub">M.S. in Electrical Engineering</div>
-          <div className="content-sub">Jan 2020 - Jun 2021</div>
+          <div className="content-date">Jan 2020 - Jun 2021</div>
         </div>
         <div className="content">
           <div className="content-title">Cornell University</div>
           <div className="content-sub">B.S. in Chemical Engineering</div>
-          <div className="content-sub">Aug 2015 - May 2019</div>
+          <div className="content-date">Aug 2015 - May 2019</div>
         </div>
       </div>
 
@@ -89,54 +74,178 @@ function App() {
         <div className="content">
           <div className="content-title">Intel Corporation</div>
           <div className="content-sub">Wafer Sort Process Engineer</div>
-          <div className="content-sub">Jul 2019 - Present</div>
+          <div className="content-date">Jul 2019 - Present</div>
         </div>
         <div className="content">
           <div className="content-title">The Hershey Company</div>
           <div className="content-sub">Research and Development Intern</div>
-          <div className="content-sub">May 2018 - Aug 2018</div>
+          <div className="content-date">May 2018 - Aug 2018</div>
         </div>
         <div className="content">
-          <div className="content-title">US Department of Agriculture - Agricultural Research Service</div>
+          <div className="content-title">USDA - Agricultural Research Service</div>
           <div className="content-sub">Physical Science Technician Intern</div>
-          <div className="content-sub">Jun 2017 - Aug 2017</div>
+          <div className="content-date">Jun 2017 - Aug 2017</div>
         </div>
       </div>
 
       <div className="app-section" id="skills">
         <div className="header">Skills</div>
-        <div className="content">
-          <div className="content-title">Programming</div>
-          <div className="content-sub">C#</div>
-          <div className="content-sub">CSS</div>
-          <div className="content-sub">HTML</div>
-          <div className="content-sub">JavaScript</div>
-          <div className="content-sub">Python</div>
-          <div className="content-sub">React</div>
-          <div className="content-sub">VBScript</div>
-          <div className="content-sub">Windows Batch Scripting</div>
-        </div>
-        <div className="content">
-          <div className="content-title">Hardware</div>
-          <div className="content-sub">Verilog</div>
-          <div className="content-sub">VHDL</div>
-        </div>
-        <div className="content">
-          <div className="content-title">Data Analysis</div>
-          <div className="content-sub">JMP/JSL</div>
-          <div className="content-sub">Matlab</div>
-          <div className="content-sub">SQLPathFinder</div>
-          <div className="content-sub">Wolfram Mathematica</div>
-        </div>
-        <div className="content">
-          <div className="content-title">Design</div>
-          <div className="content-sub">AutoCAD (2D)</div>
-          <div className="content-sub">SketchUp (3D)</div>
+        <div className="skill-row">
+          <div className="content">
+            <div className="content-title">Programming</div>
+            <div className="content-sub">C#</div>
+            <div className="content-sub">CSS</div>
+            <div className="content-sub">HTML</div>
+            <div className="content-sub">JavaScript</div>
+            <div className="content-sub">Python</div>
+            <div className="content-sub">React</div>
+            <div className="content-sub">VBScript</div>
+            <div className="content-sub">Windows Batch Scripting</div>
+          </div>
+          <div className="content">
+            <div className="content-title">Data Analysis</div>
+            <div className="content-sub">JMP/JSL</div>
+            <div className="content-sub">Matlab</div>
+            <div className="content-sub">SQLPathFinder</div>
+            <div className="content-sub">Wolfram Mathematica</div>
+          </div>
+          <div className="content">
+            <div className="content-title">Hardware</div>
+            <div className="content-sub">Verilog</div>
+            <div className="content-sub">VHDL</div>
+          </div>
+          <div className="content">
+            <div className="content-title">Design</div>
+            <div className="content-sub">AutoCAD (2D)</div>
+            <div className="content-sub">SketchUp (3D)</div>
+          </div>
         </div>
       </div>
 
       <div className="app-section" id="projects">
         <div className="header">Projects</div>
+        <div className="proj" id="queens-puzzle">
+          <div className="bg-block"></div>
+          <PhotoGallery>
+            <div 
+              src="Images/queens-puzzle.png"
+              alt="queens-puzzle"
+            />
+          </PhotoGallery>
+          <div className="project-block">
+            <div className="project-tags">
+              <div className="tag">React</div>
+              <div className="tag">JavaScript</div>
+              <div className="tag">HTML</div>
+              <div className="tag">CSS</div>
+            </div>
+            <div className="project-title">
+              Queens Puzzle
+            </div>
+            <div className="project-desc">
+              A re-creation of the "Eight Queens" puzzle with React. My first React project to
+              get to know the framework. You can receive immediate feedback if any pieces are within
+              attacking range based on the colors of the pieces.
+            </div>
+            <div className="project-links">
+              <a className="button" href="https://kaylinpunotai.github.io/queens-puzzle/" target="_blank" rel="noreferrer">Play Game</a>
+              <a className="github" href="https://github.com/kaylinpunotai/queens-puzzle" target="_blank" rel="noreferrer"><GithubIcon/></a>
+            </div>
+          </div>
+        </div>
+
+        <div className="proj" id="aws-project">
+          <div className="bg-block"></div>
+          <PhotoGallery>
+            <div 
+              src="Images/aws-dynamodb.png"
+              alt="aws-dynamodb"
+            />
+            <div 
+              src="Images/aws-lambda-test.png"
+              alt="aws-lambda-test"
+            />
+          </PhotoGallery>
+          <div className="project-block">
+            <div className="project-tags">
+              <div className="tag">Python</div>
+              <div className="tag">AWS Lambda</div>
+              <div className="tag">AWS SQS</div>
+            </div>
+            <div className="project-title">
+              AWS Connection
+            </div>
+            <div className="project-desc">
+              A school project to subscribe DynamoDB to a SQS to store incoming temperature and humidity readings. An HTML GUI 
+              can then send a GET request to the API gateway to scan and retrieve entries from the DB to the client. Lambda rules
+              are used to invoke entry creations, scans, and returns.
+            </div>
+            <div className="project-links">
+              <a className="button" href="Files/aws-part2.pdf" target="_blank" rel="noreferrer">View Details</a>
+              {/* <a className="github" href="https://github.com/kaylinpunotai/queens-puzzle" target="_blank" rel="noreferrer"><GithubIcon/></a> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="proj" id="aws-project">
+          <div className="bg-block"></div>
+          <PhotoGallery>
+            <div 
+              src="Images/temp-gui-pyqt.png"
+              alt="pyqt"
+            />
+            <div 
+              src="Images/temp-gui-html.png"
+              alt="html"
+            />
+          </PhotoGallery>
+          <div className="project-block">
+            <div className="project-tags">
+              <div className="tag">Python</div>
+            </div>
+            <div className="project-title">
+              Sensor Reading GUIs
+            </div>
+            <div className="project-desc">
+              A school project to design two GUIs (PyQT and HTML) that retrieves temperature and humidity sensor readings. 
+              The Python script retrieves readings, stores data with timestamps, calculates entry statistics, converts 
+              between Celsius and Fahrenheit, and alerts the user when readings exceed a user-defined alarm.
+            </div>
+            <div className="project-links">
+              <a className="button" href="Files/temp-gui-part1.pdf" target="_blank" rel="noreferrer">View PyQt Details</a>
+              <a className="button" href="Files/temp-gui-part2.pdf" target="_blank" rel="noreferrer">View HTML GUI Details</a>
+              {/* <a className="github" href="https://github.com/kaylinpunotai/queens-puzzle" target="_blank" rel="noreferrer"><GithubIcon/></a> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="proj" id="chessboard-3D">
+          <div className="bg-block"></div>
+          <PhotoGallery>
+            <div 
+              src="Images/3d-chessboard.png"
+              alt="3d-chessboard"
+            />
+            <div 
+              src="Images/3d-chessboard-sketches.png"
+              alt="sketches"
+            />
+          </PhotoGallery>
+          <div className="project-block">
+            <div className="project-tags">
+              <div className="tag">SketchUp</div>
+            </div>
+            <div className="project-title">
+              3D Chessboard
+            </div>
+            <div className="project-desc">
+              A chess set made and designed from scratch for a school project.
+            </div>
+            <div className="project-links">
+              <a className="button" href="https://app.connect.trimble.com/tc/api/2.0/s/IKeDasEoZmn-H2hLjnssg3SGwSm-huiiB747tpXBnVCATn4Mfd2WVrTjIYe6MRM2" target="_blank" rel="noreferrer">View Model</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/PhotoGallery.js
+++ src/PhotoGallery.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FiArrowLeftCircle, FiArrowRightCircle} from 'react-icons/fi'
+import {IoChevronBack as BackArrow, IoChevronForward as ForwardArrow} from 'react-icons/io5'
 import './PhotoGallery.css';
 
 
@@ -7,58 +7,98 @@ class PhotoGallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: 0,
-      total: 0,
+      active: 0,    // index of active photo
+      total: this.props.children.length,  // number of photos in the gallery
     };
   }
 
+  // click to go back one photo
   prevClick() {
     let curr = this.state.active;
     let total = this.state.total;
+
+    // if the index is 0, then set the index to the length of the array. when you
+    // go back one index, it will go to the final item in the array.
     if (curr===0) {
       curr = total;
     }
+
+    // set the state one index backward
     this.setState({
       active: curr - 1
     });
   }
 
+  // click to go forward one photo
   nextClick() {
     let curr = this.state.active;
     let total = this.state.total;
+
+    // if the index is the final item in the array, then set the index to -1. when
+    // you go up one index, it will go to the first item in the array.
     if (curr===total-1) {
       curr = -1;
     }
+
+    // set the state one index forward
     this.setState({
       active: curr + 1
     });
   }
 
   render() {
+    // this.props.children grabs the inner elements and their properties from where
+    // PhotoGallery is called. these will be the collection of photos for this gallery.
     const children = this.props.children;
-    var picsArray = []
+    
+    // array to keep the all the image definitions.
+    var picsArray = [];
 
-    for (let i = 0; i < children.length; i++) {
+    // show arrows to go back and forth between pictures. default is no show (as long)
+    // as only one img is present in the gallery).
+    var DisplayArrows = (<div></div>);
+
+    // if there are multiple imgs, then push a new img element into the array based on 
+    // the children's properties.
+    if (this.state.total > 1) {
+      for (let i = 0; i < children.length; i++) {
+        picsArray.push(
+          <img 
+            className="project-pics"
+            src={children[i].props.src}
+            alt={children[i].props.alt}
+          />
+        )
+      }
+
+      // add arrows to move back and forth between pictures
+      DisplayArrows = (
+        <div>
+          <BackArrow className="prev-arrow" onClick={() => this.prevClick()} />
+          <ForwardArrow className="next-arrow" onClick={() => this.nextClick()} />
+        </div>
+      )
+    }
+    // if there is only one img, then children has no index. also, no arrows need to be
+    // generated.
+    else {  
       picsArray.push(
         <img 
-          src={children[i].props.src}
-          alt={children[i].props.alt}
+          className="project-pics"
+          src={children.props.src}
+          alt={children.props.alt}
         />
       )
     }
 
-    this.setState({
-      total: picsArray.length
-    });
-
+    // set the current active state. default is 0.
     var DisplayPic = picsArray[this.state.active];
 
     return (
       <div>
-        <div className="Showcase-Carousel">
-          <DisplayPic></DisplayPic>
-          <FiArrowLeftCircle id="prev-arrow" onClick={() => this.prevClick()} />
-          <FiArrowRightCircle id="next-arrow" onClick={() => this.nextClick()} />
+        <div className="PhotoGallery">
+          {DisplayPic}
+          {DisplayArrows}
         </div>
       </div>
     );
